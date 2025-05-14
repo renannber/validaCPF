b0d2ef817f0c0d82b32811481981888213b0b211
renan berr <renannber.ads@gmail.com>
2025-05-13 22:14:51 -0300

    melhoramento do javaScript

--- cpf.js
+++ cpf.js
@@ -7,8 +7,10 @@ function validarCpf(cpf){
 
     }
     //criação da variavel 
+
     let soma = 0;
     let resto;
+
     //validação do primeriro DV, += é para guardar as informações 
     for(let i=1; i<= 9; i++){
         soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
@@ -18,7 +20,40 @@ function validarCpf(cpf){
     if (resto === 10 || resto === 11){
         resto = 0;
     }
-    
+    // resto do calculo anterios e comprando se o digito é diferente do 9 e 10 digito 
+    if(resto !== parseInt(cpf.substring(9,10))){
+        return false; 
+    }
 
-
+    //validação do segundo dv
+    soma = 0;
+    for(let i = 1;i <=10 ; i++){
+        soma += parseInt(cpf.substring(i -1, i)) * (12 - i);
+    }
+    resto = (soma * 10) % 11;
+    if(resto ===10 || resto === 11){
+        resto = 0;   
+    }
+    if(resto !== parseInt(cpf.substring(10,11))){
+        return false;
+    }
+    //valido retorna true 
+    return true;
 }
+// addeventlistener é um mecanismo para escuta 
+document.getElementById("cpfform").addEventListener("submit", function(e){
+    e.preventDefault();
+    const cpfInput =  document.getElementById("cpf").value;
+    const messageDiv =  document.getElementById("message");
+
+
+    if(validarCpf(cpfInput)){
+        messageDiv.textContent = " CPF Valido";
+        messageDiv.className = " message success";
+    } else{
+        messageDiv.textContent = "CPF invalido" ;
+        messageDiv.className=" message error";
+    }
+    messageDiv.style.display = "block";
+
+});
